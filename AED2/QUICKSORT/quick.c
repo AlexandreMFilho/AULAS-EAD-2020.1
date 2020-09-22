af9432b1548de0bdf91d7440111d6d666ab330da
Xande_WFC <Levando mochila de roda, nas costas>
2020-09-22 15:43:42 -0300

    Adicionado Quicksort

---
+++ AED2/QUICKSORT/quick.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int * Quicksort(int*,int, int);
+void Troca(int *, int, int);
+
+int main(){
+  int nome[50] = {97,108,101,120,97,110,100,114,101,109,97,105};
+  int ordenado[50];
+  strcpy(ordenado,Quicksort(nome,1,string.length(nome)));
+  printf("%s\n%s\n",nome,ordenado);
+}
+
+int* Quicksort(int*v,int e, int d){
+  if(d>e){
+    int i = e;
+    int j = d;
+    int piso = floor((e+d)/2);
+    int t = v[piso];
+    while(i<= j){
+      while (v[i] < t){
+        i+=1;
+      }while(v[j] > t){
+        j-=1;
+      }if(i<=j){
+        Troca(v,i,j);
+        i+=1;
+        j-=1;
+      }
+    }
+    Quicksort(e,j);
+    Quicksort(i,d);
+  }
+}
+
+void Troca(int *v,int i,int j){
+  int aux;
+  aux = v[i];
+  v[i] = v[j];
+  v[j] = aux;
+}
