7d570625ffeda7d28489c6f6785f5692b1ecfe46
Xande_WFC <Levando mochila de roda, nas costas>
2020-09-25 10:07:50 -0300

    Es4 AED

---
+++ AED2/Es4/Chica.c
@@ -0,0 +1,36 @@
+#include<sdtio.h>
+
+void Chica(int);
+
+int main(){
+  int num, aux;
+  for(int i;i<=n;i++){
+    printf("%d eh divisivel por 7?\n");
+    aux = Chica(i);
+    if(aux > 0){
+      printf("Sim\n");
+    }else{
+      printf("Nao\n");
+    }
+    printf("---------------\n");
+  }
+}
+
+int Chica(int n){
+  if(n == 49){
+    //ok é div
+    return 1;
+  }else if(n < 10){
+    if(n == 7){
+      //ok é div
+      return 1;
+    }else{
+      //Não é div
+      return -1;
+    }
+  }
+  int val,rest;
+  val = n/10;
+  rest = n%10;
+  return Chica(val+rest);
+}
